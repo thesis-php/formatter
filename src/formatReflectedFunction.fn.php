be6ed209670b4c1c2a903ca1e0bc1563ce57d914
MarshmallowFox <shudaof@gmail.com>
2025-05-12 23:47:51 +0500

    change from field name to method getName

--- src/formatReflectedFunction.fn.php
+++ src/formatReflectedFunction.fn.php
@@ -14,7 +14,7 @@ function formatReflectedFunction(\ReflectionFunctionAbstract $function): string
         return \sprintf('%s::%s()', formatClass($function->class), $function->name);
     }
 
-    if (!str_starts_with($function->name, '{closure')) {
+    if (!str_starts_with($function->getName(), '{closure')) {
         $class = $function->getClosureCalledClass();
 
         if ($class !== null) {
