d1c190c146ea01258d1aeba34f5d925cf38f0ac3
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2026-04-10 00:45:03 +0300

    Rescaffold

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -20,7 +20,7 @@ $config = (new Config())
     ->setCacheFile(__DIR__ . '/var/' . basename(__FILE__) . '.cache');
 
 (new PhpCsFixerCodingStandard())->applyTo($config, [
-    'new_expression_parentheses' => false,
+    // 'rule' => ['overridden' => 'config'],
 ]);
 
 return $config;
